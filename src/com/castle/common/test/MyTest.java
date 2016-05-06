2ec8dcf6475d37b672a2aba5d883b1ce1dbdead9
castle <zyyy930@126.com>
2016-05-06 22:38:43 +0800

    Print "Hello World !"

---
+++ src/com/castle/common/test/MyTest.java
@@ -0,0 +1,12 @@
+package com.castle.common.test;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world !");
+
+	}
+
+}
